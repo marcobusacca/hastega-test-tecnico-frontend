1478e33578bec56b9b2c78a83da086f72a2ee0fd
marcobusacca <marcobusacca01@gmail.com>
2024-02-14 14:45:32 +0100

    [FEAT] App.vue chiamata API GET "getUsers" con Axios

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,7 @@
 <script>
+import { store } from './store';
+import axios from 'axios';
+
 import AppHeader from './components/AppHeader.vue';
 
 export default {
@@ -8,12 +11,37 @@ export default {
   },
   data() {
     return {
-
+      store,
+      users: [],
     }
   },
-  methods: {
-
+  mounted() {
+    this.getUsers();
   },
+  methods: {
+    getUsers() {
+      // FACCIO PARTIRE IL LOADING
+      this.store.loading = true;
+
+      // FACCIO LA CHIAMATA API PER OTTENERE GLI USERS
+      axios.get(`${store.baseUrl}/api/users`).then((response) => {
+
+        // SALVO I DATI DENTRO L'ARRAY USERS
+        this.users = response.data;
+
+        // FERMO IL LOADING
+        this.store.loading = false;
+
+      }).catch((error) => {
+
+        // STAMPO IN CONSOLE L'ERRORE
+        console.error("Errore nella richiesta API: ", error);
+
+        // FERMO IL LOADING
+        this.store.loading = false;
+      });
+    }
+  }
 }
 </script>
 
--- src/store.js
+++ src/store.js
@@ -3,6 +3,6 @@ import { reactive } from "vue";
 
 // ESPORTO LA CONSTANTE STORE CONTENENTE TUTTI GLI ELEMENTI ACCESSIBILI AD OGNI COMPONENTE VUE
 export const store = reactive({
-    baseUrl: 'http://127.0.0.1:8000',
+    baseUrl: 'http://127.0.0.1:8080',
     loading: true,
 });
