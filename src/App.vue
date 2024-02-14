dff0ccab35c950ea646ec95deca489ca4ed47e30
marcobusacca <marcobusacca01@gmail.com>
2024-02-14 12:54:59 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+
+  components: {
+    AppHeader,
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+}
+</script>
+
+<template>
+  <app-header />
+</template>
+
+<style lang="scss">
+// IMPORTO GENERALS.SCSS
+@use './styles/generals.scss' as *;
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+    <header>
+        <!-- Header Navbar -->
+        <nav class="bg-black text-white py-4">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12 text-center">
+                        <h1>HASTEGA TEST TECNICO</h1>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
+</template>
+
+<style lang="scss" scoped></style>
